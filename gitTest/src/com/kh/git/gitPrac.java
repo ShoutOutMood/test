f485f9cd1c2c63a7493b6b4b20c5692decb7daa0
user1 <user1@DESKTOP-G0085KH>
2020-04-24 12:21:03 +0900

    first

---
+++ gitTest/src/com/kh/git/gitPrac.java
@@ -0,0 +1,15 @@
+package com.kh.git;
+
+public class gitPrac {
+
+	public static void main(String[] args) {
+             
+		System.out.println("JMoodMusic"); 
+		System.out.println("Doing gitHub~");
+		System.out.println("Chillout");
+		System.out.println("ambient");
+		System.out.println("난 조장"); 
+		// 올라감? 
+	}
+
+}
